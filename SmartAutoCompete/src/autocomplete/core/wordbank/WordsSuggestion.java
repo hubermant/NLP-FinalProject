4a3ea53684f4fa1714d91649069b18dd1b375115
Tom Huberman <hubermant@gmail.com>
2013-06-22 16:43:52 +0200

    commiting for updating WordRanl

--- SmartAutoCompete/src/autocomplete/core/wordbank/WordsSuggestion.java
+++ SmartAutoCompete/src/autocomplete/core/wordbank/WordsSuggestion.java
@@ -28,11 +28,37 @@ public class WordsSuggestion {
 	/* ---- Public Methods ---- */
 	
 	public void put(List<String> features, String word) {
-		
+		if (!wordToFrequency.containsKey(word)) {
+			wordToFrequency.put(word, 0);
+		}
+		wordToFrequency.put(word, wordToFrequency.get(word) + 1);
+		if (!featuresToWords.containsKey(features)) {
+			featuresToWords.put(features, new TreeMap<String, WordRank>());
+		}
+		if (!featuresToWords.get(features).containsKey(word)) {
+			featuresToWords.get(features).put(word, new WordRank(word));
+		}
+		WordRank wordRank = featuresToWords.get(features).get(word);
+		wordRank.inc();
 	}
 	
 	public List<WordRank> get(List<String> features, String prefix, int resultNum) {
-		return null;
+		if (featuresToWords.containsKey(features)) {
+			SortedMap<String,WordRank> filteredMap = filterPrefix(featuresToWords.get(features), prefix);
+			return null;
+		} else {
+			//TODO: need to think what to do in case where the features never appeared before. 
+			return null;
+		}
+	}
+	
+	public SortedMap<String, WordRank> filterPrefix(SortedMap<String,WordRank> baseMap, String prefix) {
+	    if(prefix.length() > 0) {
+	        char nextLetter = (char) (prefix.charAt(prefix.length() -1) + 1);
+	        String end = prefix.substring(0, prefix.length()-1) + nextLetter;
+	        return baseMap.subMap(prefix, end);
+	    }
+	    return baseMap;
 	}
 
 }
--- SmartAutoCompete/src/utill/collection/WordRank.java
+++ SmartAutoCompete/src/utill/collection/WordRank.java
@@ -1,12 +1,28 @@
 package utill.collection;
 
-public class WordRank {
+public class WordRank implements Comparable<WordRank> {
 
 	/* ---- Data Members ---- */
 	
+	public WordRank(String word) {
+		this.word = word;
+		counter = 0;
+	}
+
 	private String word;
 	
-	private double probability;
-	
 	private int counter;
+
+
+	public void inc() {
+		counter++;
+	}
+
+
+	@Override
+	public int compareTo(WordRank o) {
+		if (word.equals(o))
+		
+		return 0;
+	}
 }
