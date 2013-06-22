f6bcd0654b5b9f4132b26c5344d79724d8ff7dde
Dan Evgi <danevgi@gmail.com>
2013-06-22 16:49:33 +0300

    add more

--- tests/autocomplete/core/wordbank/WordsSuggestionTest.java
+++ tests/autocomplete/core/wordbank/WordsSuggestionTest.java
@@ -25,8 +25,25 @@ public class WordsSuggestionTest {
 		assertEquals("baz", res.get(0).getWord());
 		assertEquals(2, res.get(0).getCounter());
 		
-		assertEquals("blah", res.get(0).getWord());
-		assertEquals(1, res.get(0).getCounter());
+		assertEquals("blah", res.get(1).getWord());
+		assertEquals(1, res.get(1).getCounter());
+	}
+	
+	@Test
+	public void testUnkownFeatures() {
+		WordsSuggestion ws = new WordsSuggestion();
+		
+		List<WordRank> res = ws.get(Arrays.asList("foo", "bar"), "", 3);
+		assertEquals(0, res.size());
+	}
+	
+	@Test
+	public void testUnkownPrefix() {
+		WordsSuggestion ws = new WordsSuggestion();
+		
+		ws.put(Arrays.asList("foo", "bar"), "baz");
+		List<WordRank> res = ws.get(Arrays.asList("foo", "bar"), "zoo", 3);
+		assertEquals(0, res.size());
 	}
 
 }
