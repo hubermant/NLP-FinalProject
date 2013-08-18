bac5a9728c5c38d1b3ec4932928634ff681ef599
Dan Evgi <danevgi@gmail.com>
2013-08-18 23:55:19 +0300

    add more resutls add the test that made them

--- SmartAutoCompete/tests/autocomplete/io/parse/irc/IRCParserTest.java
+++ SmartAutoCompete/tests/autocomplete/io/parse/irc/IRCParserTest.java
@@ -8,8 +8,10 @@ import java.io.Reader;
 
 import org.junit.Test;
 
+import autocomplete.core.completer.BasicCompleter;
 import autocomplete.core.completer.Completer;
 import autocomplete.core.completer.IRCCompleter;
+import autocomplete.core.completer.LearningCompleter;
 
 public class IRCParserTest {
 	
@@ -19,9 +21,45 @@ public class IRCParserTest {
 		Reader trainReader2 = new FileReader("resources/#wikipedia.20090731.log.irc");
 		Reader trainReader3 = new FileReader("resources/#wikipedia-en.20100103.log.irc");
 		Reader testReader = new FileReader("resources/#wikipedia-en.20100117.log.irc");
-		OutputStreamWriter writer = new FileWriter("resources/res-irc.txt");
+		OutputStreamWriter writer = new FileWriter("results/res-irc.txt");
 		
 		Completer completer = new IRCCompleter(1, 3, 4); 
+		IRCParser parser = new IRCParser(completer);
+		System.out.println("start training");
+		parser.train(trainReader1);
+		parser.train(trainReader2);
+		parser.train(trainReader3);
+		System.out.println("done training");
+		parser.complete(testReader, writer);
+	}
+	
+	@Test
+	public void testIRCParserLearning() throws IOException {
+		Reader trainReader1 = new FileReader("resources/##chemistry.20100116.log.irc");
+		Reader trainReader2 = new FileReader("resources/#wikipedia.20090731.log.irc");
+		Reader trainReader3 = new FileReader("resources/#wikipedia-en.20100103.log.irc");
+		Reader testReader = new FileReader("resources/#wikipedia-en.20100117.log.irc");
+		OutputStreamWriter writer = new FileWriter("results/res-irc-learning-1gram.txt");
+		
+		Completer completer = new LearningCompleter(1, 3); 
+		IRCParser parser = new IRCParser(completer);
+		System.out.println("start training");
+		parser.train(trainReader1);
+		parser.train(trainReader2);
+		parser.train(trainReader3);
+		System.out.println("done training");
+		parser.complete(testReader, writer);
+	}
+
+	@Test
+	public void testIRCParserBaseline() throws IOException {
+		Reader trainReader1 = new FileReader("resources/##chemistry.20100116.log.irc");
+		Reader trainReader2 = new FileReader("resources/#wikipedia.20090731.log.irc");
+		Reader trainReader3 = new FileReader("resources/#wikipedia-en.20100103.log.irc");
+		Reader testReader = new FileReader("resources/#wikipedia-en.20100117.log.irc");
+		OutputStreamWriter writer = new FileWriter("results/res-irc-baseline-1gram.txt");
+		
+		Completer completer = new BasicCompleter(1, 3); 
 		IRCParser parser = new IRCParser(completer);
 		System.out.println("start training");
 		parser.train(trainReader1);
