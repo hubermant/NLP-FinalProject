e5a5dd0756bc7b6e425028861a26ad4ebec5a82e
Tom Huberman <hubermant@gmail.com>
2013-08-23 14:47:32 +0200

    no message

--- SmartAutoCompete/src/autocomplete/core/wordbank/WordsSuggestion.java
+++ SmartAutoCompete/src/autocomplete/core/wordbank/WordsSuggestion.java
@@ -8,26 +8,39 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import autocomplete.core.completer.Completer;
+
 import utill.collection.WordRank;
 
+/**
+ * This class contains all the data the completer learns, 
+ * the number of occurrences each word had, 
+ * the number of occurrences of each word after a certain ngram,
+ * and total number of words.
+ * 
+ * @see Completer
+ * @see WordRank
+ */
 public class WordsSuggestion {
+	
 	
 	/* ---- Data Members ---- */
 	
+	/** {@link Map} between the n-gram words to a sorted map of the word to there occurrences. */
 	private Map<List<String>,SortedMap<String, WordRank>> featuresToWords;
 	
+	/** The number of time each word appeared. */
 	private Map<String, WordRank> wordToFrequency;
 	
+	/** The total number of words. */
 	private long totalNumberOfWords = 0;
 	
 	
 	/* ---- Constructors ---- */
-	
-	public long getTotalNumberOfWords() {
-		return totalNumberOfWords;
-	}
 
-
+	/**
+	 * Default constructor.
+	 */
 	public WordsSuggestion() {
 		featuresToWords = new HashMap<List<String>, SortedMap<String,WordRank>>();
 		wordToFrequency = new HashMap<String, WordRank>();
@@ -36,6 +49,12 @@ public class WordsSuggestion {
 	
 	/* ---- Public Methods ---- */
 	
+	/**
+	 * update all the occurrences after this word was seen after this specific n-gram.
+	 *  
+	 * @param features The n-gram.
+	 * @param word The word that appeared after that n-gram.
+	 */
 	public void put(List<String> features, String word) {
 		if (!wordToFrequency.containsKey(word)) {
 			wordToFrequency.put(word, new WordRank(word));
@@ -52,6 +71,12 @@ public class WordsSuggestion {
 		totalNumberOfWords++;
 	}
 	
+	/**
+	 * This method update all the counters of this word amount times.
+	 * 
+	 * @param word The word needed to be updated.
+	 * @param amount The amount of occurrences to update.
+	 */
 	public void putAmount(String word, int amount) {
 		for (List<String> features : featuresToWords.keySet()) {
 			if (featuresToWords.get(features).containsKey(word)) {
@@ -60,13 +85,25 @@ public class WordsSuggestion {
 		}
 	}
 
-
+	/**
+	 * Update all the occurrences after this word was seen after this specific n-gram, amount times.
+	 * 
+	 * @param word The word that appeared after that n-gram.
+	 * @param amount The amount of times to add to the occurrences.
+	 * @param features The n-gram.
+	 */
 	public void putAmount(String word, int amount, List<String> features) {
 		for(int i = 0; i < amount; i++) {
 			put(features, word);
 		}
 	}
 	
+	/**
+	 * This method is used after a session to reduce the number of occurrences.
+	 * 
+	 * @param word The word needed to be update.
+	 * @param amount The amount need to be reduce.
+	 */
 	public void removeAmount(String word, int amount) {
 		for (SortedMap<String, WordRank> map : featuresToWords.values()) {
 			if (map.containsKey(word)) {
@@ -77,6 +114,17 @@ public class WordsSuggestion {
 		} 
 	}
 	
+	/**
+	 * This method return exactly resultNum suggestions for completion.
+	 * 
+	 * @param features The n-gram.
+	 * @param prefix The current word prefix.
+	 * @param resultNum The number of wanted result.
+	 * @return Suggestions for the current word.
+	 * 
+	 * @deprecated This method is used only in the tests, the cut of the result number is now in the completer. 
+	 */
+	@Deprecated
 	public List<WordRank> get(List<String> features, String prefix, int resultNum) {
 		List<WordRank> segustions = getByFeetchersAndPrefix(features, prefix); 
 		if (segustions.size() < resultNum) {
@@ -86,6 +134,30 @@ public class WordsSuggestion {
 		}
 	}
 
+	/**
+	 * filter the {@link SortedMap} for only the words start with the given prefix.
+	 * 
+	 * @param baseMap The base map.
+	 * @param prefix The prefix that used to filter the map.
+	 * @return A {@link Map} contains only words start with the given prefix.
+	 */
+	public SortedMap<String, WordRank> filterPrefix(SortedMap<String,WordRank> baseMap, String prefix) {
+	    if(prefix.length() > 0) {
+	        char nextLetter = (char) (prefix.charAt(prefix.length() -1) + 1);
+	        String end = prefix.substring(0, prefix.length()-1) + nextLetter;
+	        return baseMap.subMap(prefix, end);
+	    }
+	    return baseMap;
+	}
+
+
+	/**
+	 * Return all the possible words given this n-gram and this prefix.
+	 * 
+	 * @param features The n-gram.
+	 * @param prefix The word prefix.
+	 * @return All the possible suggestions.
+	 */
 	public List<WordRank> getByFeetchersAndPrefix(List<String> features, String prefix) {
 		ArrayList<WordRank> segustions = new ArrayList<>(wordToFrequency.values());
 		if (featuresToWords.containsKey(features)) {
@@ -97,17 +169,25 @@ public class WordsSuggestion {
 		return segustions;
 	}
 	
+	
+	/* ---- Getters --- */
+	
+	/**
+	 * This is a getter for the {@link WordsSuggestion#totalNumberOfWords}
+	 * 
+	 * @return The total number of words appeared.
+	 */
+	public long getTotalNumberOfWords() {
+		return totalNumberOfWords;
+	}
+	
+	/**
+	 * This is a getter for the {@link WordsSuggestion#wordToFrequency}
+	 * 
+	 * @return A {@link Map} between each word to the number of occurrences.
+	 */
 	public Map<String, WordRank> getWordCountMap() {
 		return wordToFrequency;
 	}
 	
-	public SortedMap<String, WordRank> filterPrefix(SortedMap<String,WordRank> baseMap, String prefix) {
-	    if(prefix.length() > 0) {
-	        char nextLetter = (char) (prefix.charAt(prefix.length() -1) + 1);
-	        String end = prefix.substring(0, prefix.length()-1) + nextLetter;
-	        return baseMap.subMap(prefix, end);
-	    }
-	    return baseMap;
-	}
-
 }
